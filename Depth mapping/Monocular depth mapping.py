91113ee3d3c7855732d8dc335ccccce2dd65bcf6
lucas <lucasbry10@gmail.com>
2025-07-15 17:33:04 +0100

    Working on depth estimation

--- Depth mapping/Monocular depth mapping.py
+++ Depth mapping/Monocular depth mapping.py
@@ -1,8 +1,5 @@
 import cv2
 
-# Import the Haar Cascade classifier for face detection https://github.com/opencv/opencv/tree/master/data/haarcascades
-face_classifier = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml") 
-
 """
 # Open the video stream from the RTSP URL
 # RPi cmd: libcamera-vid -t 0 -n --low-latency --framerate 20 --profile high --intra 10 --codec libav --libav-format mpegts -o - | cvlc stream:///dev/stdin --sout '#rtp{sdp=rtsp://:8554/stream1}'
@@ -21,17 +18,8 @@ gst_str = (
 video = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
 """
 
-# Open the video stream from the camera
-video = cv2.VideoCapture(0)  # Change to 0 for webcam or use
-
-def detect_faces(frame):
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_classifier.detectMultiScale(gray_frame, scaleFactor=1.1, minNeighbors=5, minSize=(50, 50))
-    
-    for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
-    
-    return frame
+# Open the video stream from webcam
+video = cv2.VideoCapture(0)
 
 while True:
     ret, frame = video.read()
@@ -39,9 +27,7 @@ while True:
         print("Can't receive frame (stream end?). Exiting ...")
         break
 
-    faces = detect_faces(frame)
-
-    cv2.imshow("Face Detection", faces)
+    cv2.imshow("Video Stream", frame)
 
     if cv2.waitKey(1) == ord('q'):
         break
